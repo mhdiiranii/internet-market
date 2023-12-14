4f05f28569a4d2b1e73551ffda843a1af05b56d8
mehdi <mhdiiranii@gmail.com>
2023-12-15 01:48:33 +0330

    add animation

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,8 +13,67 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
-    },
-  },
+      animation: {
+        'text-slide': 'text-slide 3s linear ',
+        'op':'op 2s ease-in infinite',
+        'sliderRight':'sliderRight 0.75s linear',
+        'sliderLeft':'sliderLeft 0.75s linear'
+      },
+      keyframes: {
+        'text-slide': {
+            '0%, 25%':{
+              transform: 'translateX(100%)',
+              opacity:'0',
+            },
+
+            '25%, 50%':{
+              transform: 'translateX(100%)',
+              opacity:'0',
+            },
+            '50%, 100%': {
+                transform: 'translateX(0%)',
+                opacity:'1',          
+            },
+        },
+        "op":{
+            '0%, 25%':{
+              boxShadow :'1px 1px 35px rgba(255,255,255,0)'
+            },
+            '25%, 50%':{
+              boxShadow :'1px 1px 35px rgba(255,255,255,0.5)'
+            },
+            '50%, 75%':{
+              boxShadow :'1px 1px 35px rgba(255,255,255,0.5)'
+            },
+            '75%, 100%':{
+              boxShadow :'1px 1px 35px rgba(255,255,255,0)'
+            }
+        },  
+        'sliderRight':{
+            // '0%, 25%':{
+            //   transform: 'translateX(-100%)',
+            // },
+
+            '0%, 50%':{
+              transform: 'translateX(-100%)',
+            },
+            '50%, 100%': {
+                transform: 'translateX(0%)',       
+            },
+        },
+        'sliderLeft':{
+          // '0%, 25%':{
+          //     transform: 'translateX(100%)',
+          //   },
+
+            '0%, 50%':{
+              transform: 'translateX(100%)',
+            },
+            '50%, 100%': {
+                transform: 'translateX(0%)',         
+            }
+        }
+      },
   plugins: [],
-}
+}}}
 export default config
