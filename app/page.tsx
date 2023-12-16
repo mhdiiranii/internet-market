f366e92adad9f585ac79ed17a97bdb0512a28c89
mehdi <mhdiiranii@gmail.com>
2023-12-16 14:00:31 +0330

    home

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
 import Image from 'next/image'
 import SliderImage from './(block)/SliderImage'
+import DiscountItem from './(block)/DiscountProduct'
 
 export default function Home() {
   return (
-    <SliderImage/>
+    <>
+      <SliderImage/>
+      <DiscountItem/>
+    </>
   )
 }
