47c422518dc815a4693ae036b5d325776327874e
mehdi <mhdiiranii@gmail.com>
2023-12-16 13:59:04 +0330

    redux provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import ReduxProider from '@/Redux/provider'
+import Header from './(block)/Header'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -19,6 +20,7 @@ export default function RootLayout({
     <html lang="en">
       <ReduxProider>
         <body className={inter.className}>
+          <Header/>
           {children}
         </body>
       </ReduxProider>
