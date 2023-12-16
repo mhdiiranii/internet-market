c3efd0134ee5f35e363a2f891a44b12ff0a46ce6
mehdi <mhdiiranii@gmail.com>
2023-12-16 13:59:38 +0330

    all type

--- app/type.ts
+++ app/type.ts
@@ -4,3 +4,25 @@ export type sliderImageType = Array<{
     id:number,
     src:StaticImageData
 }>
+
+export type discountProductType = Array<{
+    id:number,
+    src:string,
+    price:string,
+    priceDiscount:string,
+    previousPrice:string
+}>
+
+export interface cartType {
+    src : string,
+}
+export type myAllCartType = Array<{
+    id:number,
+    src : string,
+}>
+
+export type myNavType = Array<{
+    id:number,
+    title:string,
+    href?:any
+}>
