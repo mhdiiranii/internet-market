b8eb803c3eda70345333ed6bad6e1c60feb2f938
mehdi <mhdiiranii@gmail.com>
2023-12-19 20:47:01 +0330

    root page add grouping

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import Image from 'next/image'
 import SliderImage from './(block)/SliderImage'
 import DiscountItem from './(block)/DiscountProduct'
+import  Grouping from './(block)/Grouping'
 
 export default function Home() {
   return (
     <>
       <SliderImage/>
       <DiscountItem/>
+      <Grouping/>
     </>
   )
 }
